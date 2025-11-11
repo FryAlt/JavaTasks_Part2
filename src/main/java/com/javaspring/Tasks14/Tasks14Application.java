b18417c40def6d1a55ffbe23ce31b88677dc3325
FryAlt <67910082+FryAlt@users.noreply.github.com>
2025-11-12 01:59:42 +0500

    start-up Java Spring

---
+++ src/main/java/com/javaspring/Tasks14/Tasks14Application.java
@@ -0,0 +1,13 @@
+package com.javaspring.Tasks14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tasks14Application {
+
+    public static void main(String[] args) {
+		SpringApplication.run(Tasks14Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaspring/Tasks14/Tasks14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaspring.Tasks14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tasks14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
